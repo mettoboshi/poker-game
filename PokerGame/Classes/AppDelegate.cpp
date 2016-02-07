2d412fdf829434e6e8e44c9aca5349ccb737f00d
Yasuo Fujita <mettoboshi.1108@gmail.com>
2016-02-07 16:41:06 +0900

    【第4講】4-5 シーンを呼び出そう

--- PokerGame/Classes/AppDelegate.cpp
+++ PokerGame/Classes/AppDelegate.cpp
@@ -1,5 +1,5 @@
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+#include "TitleScene.hpp"
 
 USING_NS_CC;
 
@@ -42,7 +42,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     FileUtils::getInstance()->addSearchPath("res");
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    Scene* scene { TitleScene::createScene() };
 
     // run
     director->runWithScene(scene);
