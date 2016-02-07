0ddb95343ce7ab4d761092b9127ff66ec7ce11d8
Yasuo Fujita <mettoboshi.1108@gmail.com>
2016-02-07 16:37:19 +0900

    【第4講】4-3 クラスの雛形を作る

--- PokerGame/Classes/TitleScene.cpp
+++ PokerGame/Classes/TitleScene.cpp
@@ -4,3 +4,33 @@
 //
 
 #include "TitleScene.hpp"
+
+USING_NS_CC;
+
+Scene* TitleScene::createScene()
+{
+    // 'scene' is an autorelease object
+    Scene* scene { Scene::create() };
+    
+    // 'layer' is an autorelease object
+    Layer* layer { TitleScene::create() };
+    
+    // add layer as a child to scene
+    scene->addChild(layer);
+    
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool TitleScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    return true;
+}
--- PokerGame/Classes/TitleScene.hpp
+++ PokerGame/Classes/TitleScene.hpp
@@ -3,9 +3,22 @@
 //  PokerGame
 //
 
-#ifndef TitleScene_hpp
-#define TitleScene_hpp
+#ifndef __PokerGame__TitleScene__
+#define __PokerGame__TitleScene__
 
-#include <stdio.h>
+#include "cocos2d.h"
 
-#endif /* TitleScene_hpp */
+class TitleScene : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+    
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(TitleScene);
+};
+
+#endif /* defined(__PokerGame__TitleScene__) */
