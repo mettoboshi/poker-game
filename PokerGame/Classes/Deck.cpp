694ce6855f545f293e1ccd10a534f280f1bae9f2
Yasuo Fujita <mettoboshi.1108@gmail.com>
2016-02-08 02:28:26 +0900

    【第7講】7-4 デッキクラスの作成

---
+++ PokerGame/Classes/Deck.cpp
@@ -0,0 +1,48 @@
+//
+//  Deck.cpp
+//  PokerGame
+//
+
+#include "Deck.hpp"
+
+Deck::Deck()
+{};
+
+Deck::~Deck()
+{}
+
+bool Deck::init()
+{
+    this->setAllCard();
+    
+    return true;
+}
+
+// デッキにカードをセット
+void Deck::setAllCard() {
+
+    // デッキの中身を空にする
+    this->deckData.clear();
+    this->deckData.shrinkToFit();
+    
+    // デッキにカードをセットする
+    for (int i { 1 }; i <= 13; i++) {
+        this->deckData.pushBack(Card::create(Suit::SPADE, i));
+        this->deckData.pushBack(Card::create(Suit::HEART, i));
+        this->deckData.pushBack(Card::create(Suit::DIAMOND, i));
+        this->deckData.pushBack(Card::create(Suit::CLUB, i));
+    }
+}
+
+// カードを１枚配る
+Card* Deck::dealCard() {
+
+    // 先頭のカードを取得
+    Card* card = this->deckData.front();
+    CC_SAFE_RETAIN(card);
+
+    // デッキからカードを削除
+    this->deckData.eraseObject(card);
+    
+    return card;
+}
---
+++ PokerGame/Classes/Deck.hpp
@@ -0,0 +1,35 @@
+//
+//  Deck.hpp
+//  PokerGame
+//
+
+#ifndef __PokerGame__Deck__
+#define __PokerGame__Deck__
+
+#include "Common.h"
+#include "Card.hpp"
+
+class Deck : public cocos2d::Ref
+{
+// ---------- クラスメソッド
+public:
+    CREATE_FUNC(Deck);
+
+// ---- インスタンス変数
+private:
+    cocos2d::Vector<Card*> deckData;
+
+// ----- インスタンスメソッド
+private:
+    Deck();  // コンストラクタ
+    ~Deck(); // デストラクタ
+
+    bool init(); // 初期化
+
+public:
+    void setAllCard(); // デッキにカードをセット
+    Card* dealCard();  // カードを１枚配る
+    
+};
+
+#endif /* defined(__PokerGame__Deck__) */
